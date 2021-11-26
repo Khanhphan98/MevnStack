4c8ea4fdeb7cdccd57f9f96d9424abab9f4fa7c2
khanhphan <khanhphan9898@gmail.com>
2021-11-26 08:45:17 +0700

    khanhcm

--- client/src/helpers/auth-header.js
+++
@@ -1,8 +0,0 @@
-export function authHeader(){
-    let token = JSON.parse(localStorage.getItem('token'))
-    if (user && token) {
-        return { 'Authorization': 'Bearer' + token }
-    } else {
-        return {}
-    }
-}
